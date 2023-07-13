0f139cedc41b6a30234b841a969d9bf7402783a5
johan <johansosa203@gmail.com>
2023-07-12 21:05:34 -0500

    update Link to home in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="container-fluid bg-light">
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-          <li className="nav-item"><Link to="/home" className="nav-link px-2 text-body-secondary">Home</Link></li>
+          <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
           <li className="nav-item"><Link to="/menu" className="nav-link px-2 text-body-secondary">Menu</Link></li>
           <li className="nav-item"><Link to="/aboutus" className="nav-link px-2 text-body-secondary">About Us</Link></li>
         </ul>
